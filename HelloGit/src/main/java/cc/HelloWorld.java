dd15c85ee685c8f67f2e88f7c48cd7a8f33732c7
ChenCheng <cc6666.@qq.com>
2020-11-04 00:34:34 +0800

    添加666

--- HelloGit/src/main/java/cc/HelloWorld.java
+++ HelloGit/src/main/java/cc/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("分支添加");
+        System.out.println("添加666在第7行");
+
     }
 }
